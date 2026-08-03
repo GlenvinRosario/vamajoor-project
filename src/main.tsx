ab55a079a52f34a60cdc79ef4cb59da838a4bc89
GlenvinRosario <glenrosario84@gmail.com>
2026-08-03 23:57:35 +0530

    [Glen] updated routes for supabase

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { EventProvider } from "./context/EventContext.tsx";
+
 
 createRoot(document.getElementById("root")!).render(
 <>
-<EventProvider><App /></EventProvider>
+<App />
 </>);
